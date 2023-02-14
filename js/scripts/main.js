a8c6b9cab00be69f4fc251d99ef00c81d187e003
Fabiano Guimarães <guimaraes_fabiano11@hotmail.com>
2023-02-13 22:36:42 -0300

    feat: add api fetch functions

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -9,22 +9,101 @@ var slide_hero = new Swiper(".slide-hero", {
 const cardPokemon  = document.querySelectorAll('.js-open-details-pokemon');
 const btnCloseModal = document.querySelector('.js-close-modal-details-pokemon');
 const countPokemons = document.getElementById('js-count-pokemons');
-
-cardPokemon.forEach(card => {
-  card.addEventListener('click', openDetailsPokemon);
-})
+const areaPokemons = document.getElementById('js-list-pokemons');
+const btnDropdownSelect = document.querySelector('.js-open-select-custom');
+const areaTypes = document.getElementById('js-type-area');
 
 if(btnCloseModal) {
   btnCloseModal.addEventListener('click', closeDetailsPokemon);
 }
 
-const btnDropdownSelect = document.querySelector('.js-open-select-custom');
-
 btnDropdownSelect.addEventListener('click', () => {
   btnDropdownSelect.parentElement.classList.toggle('active');
 })
 
-const areaPokemons = document.getElementById('js-list-pokemons');
+function capitalFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function createCardPokemon(code, type, name, imagePoke) {
+  let card = document.createElement('button');
+  card.classList = `card-pokemon js-open-details-pokemon ${type}`;
+  areaPokemons.appendChild(card);
+
+  let image = document.createElement('div');
+  image.classList = 'image';
+  card.appendChild(image);
+
+  let imageSrc = document.createElement('img');
+  imageSrc.className = 'thumb-img'
+  imageSrc.setAttribute('src', imagePoke);
+  image.appendChild(imageSrc);
+
+  let infoCardPokemon = document.createElement('div');
+  infoCardPokemon.classList = 'info';
+  card.appendChild(infoCardPokemon);
+
+  let infoTextPokemon = document.createElement('div');
+  infoTextPokemon.classList = 'text';
+  infoCardPokemon.appendChild(infoTextPokemon);
+
+  let codePokemon = document.createElement('span');
+  codePokemon.textContent = (code < 10) ? `#00${code}` : (code < 100) ? `#0${code}` : `#${code}`;
+  infoTextPokemon.appendChild(codePokemon);
+
+  let namePokemon = document.createElement('h3');
+  namePokemon.textContent = capitalFirstLetter(name);
+  infoTextPokemon.appendChild(namePokemon);
+
+  let areaIcon = document.createElement('div');
+  areaIcon.classList = 'icon';
+  infoCardPokemon.appendChild(areaIcon);
+
+  let imgType = document.createElement('img');
+  imgType.setAttribute('src', `img/icon-types/${type}.svg`);
+  areaIcon.appendChild(imgType);
+}
+
+function listingPokemons(urlApi) {
+  axios({
+    method: 'GET',
+    url: urlApi
+  })
+  .then((response) => {
+    const countPokemons = document.getElementById('js-count-pokemons');
+    const { results, next, count } = response.data;
+    countPokemons.innerText = count;
+
+    results.forEach(pokemon => {
+      let urlApiDetails = pokemon.url;
+
+      axios({
+        method: 'GET',
+        url: `${urlApiDetails}`
+      })
+      .then((response) => {
+        const { name, id, sprites, types } = response.data;
+
+        const infoCard = {
+          name: name,
+          code: id,
+          image: sprites.other.dream_world.front_default,
+          type: types[0].type.name
+        }
+
+        createCardPokemon(infoCard.code, infoCard.type, infoCard.name, infoCard.image);
+
+        const cardPokemon  = document.querySelectorAll('.js-open-details-pokemon');
+
+        cardPokemon.forEach(card => {
+          card.addEventListener('click', openDetailsPokemon);
+        })
+      })
+    })
+  })
+}
+
+listingPokemons('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0')
 
 function openDetailsPokemon() {
   document.documentElement.classList.add('open-modal');
@@ -34,6 +113,39 @@ function closeDetailsPokemon() {
   document.documentElement.classList.remove('open-modal');
 }
 
+// Aqui é o script para listar todos os tipos de pokemon
+
+axios({
+  method: 'GET',
+  url: 'https://pokeapi.co/api/v2/type'
+})
+.then(response => {
+  const { results } = response.data;
+  
+  results.forEach((type, index) => {
+
+    if(index < 18) {
+      let itemType = document.createElement('li');
+      areaTypes.appendChild(itemType);
+  
+      let buttonType = document.createElement('button');
+      buttonType.classList = `type-filter ${type.name}`
+      itemType.appendChild(buttonType);
+  
+      let iconType = document.createElement('div');
+      iconType.classList = 'icon';
+      buttonType.appendChild(iconType);
+  
+      let srcType = document.createElement('img');
+      srcType.setAttribute('src', `img/icon-types/${type.name}.svg`);
+      iconType.appendChild(srcType);
+
+      let nameType = document.createElement('span');
+      nameType.textContent = capitalFirstLetter(type.name);
+      buttonType.appendChild(nameType);
+    }
+  })
+})
 
 
 
